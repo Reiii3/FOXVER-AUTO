8817adfce82355a4ecf444636f1e4fea42d70b1e
Reiii3 <mohtoyib79@gmail.com>
2025-03-14 05:09:59 +0700

    update rmv.sh

--- rmv.sh
+++ rmv.sh
@@ -1,1 +1,18 @@
+$AXFUN
+bin="/data/local/tmp/fxver"
+path="/data/locla/tmp/axeron_cash/FOX"
+prop="$bin/prop"
+. $prop
 
+echo "=========================="
+echo "  $name | AI REMOVED"
+echo "=========================="
+sleep 1
+echo "  ┌[📦] Remove Modules System"
+echo "  ├[🖲] Game : $nameGame"
+echo "  └┬[📄] Version : $vers | $versc"
+echo "   └[⚠️] Status System : Removed "
+echo "  ©️Copyright By Reii"
+sleep 1
+rm -rf "$bin"
+rm -rf "$path"
