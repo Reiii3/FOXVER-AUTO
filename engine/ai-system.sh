ac83b790478f22f4f7f8a87cf75e67ee87237873
Reiii3 <mohtoyib79@gmail.com>
2025-03-22 06:05:12 +0700

    update engine/ai-system.sh

--- engine/ai-system.sh
+++ engine/ai-system.sh
@@ -42,6 +42,7 @@ check_game() {
 detected_apps=$(dumpsys window | grep -E 'mCurrentFocus|mFocusedApp' | grep -o "$packageRun")
 render_detected=$(getprop debug.hwui.renderer)
 perfo=$(echo "$tes_up" | grep -q "cmd settings put global high_performance_mode_on=1|0" && echo "$tes_up" grep -q "cmd settings put global high_performance_mode_on_when_shutdown=1|0")
+. $prop
     if [ -n "$detected_apps" ]; then
         if [ "$gamerun" != "running" ] || [ "$render_detected" != "skiavk" ]; then
             ai_start
