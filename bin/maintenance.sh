6ca9cd15f491cabb5e95d56110c7fd9e3aabdae2
Reiii3 <mohtoyib79@gmail.com>
2025-03-21 12:49:36 +0700

    update bin/maintenance.sh

--- bin/maintenance.sh
+++ bin/maintenance.sh
@@ -1,1 +1,23 @@
+$AXFUN
+import axeron.prop
+local war="[?]"
+local in="[!]"
+local pr="[-]"
+local su="[✔]"
+
 echo "Minetenance Tess"
+echo "      ========================"
+echo "        Maintenance V1.1 XIO"
+echo "      ========================"
+echo "        $name | Information"
+echo "- Maintenance in 21 Mar 2025 12.30 WIB"
+echo " - Update Version 1.1-XIO | 11"
+echo "    ========================"
+ printer "  MAINTENANCE 1.6 BETA"
+    echo "========================"
+    printer "  [WHAT'S UPDATED]  "
+    printer "  - New Function  "
+    printer "  - Add New Code  "
+    printer "  - Fix Logic Modules  "
+    printer "  - System Modules Update  "
+    echo
