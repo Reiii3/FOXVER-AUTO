b588a8ca5e0f52722261f86aff4b1ba840ed911f
Reiii3 <mohtoyib79@gmail.com>
2025-03-22 04:48:44 +0700

    update bin/changelogs.sh

--- bin/changelogs.sh
+++ bin/changelogs.sh
@@ -1,1 +1,21 @@
-echo "Changelogs Tess"
+$AXFUN
+import axeron.prop
+local bin="/data/local/tmp/fxver"
+local fun="$bin/function"
+local prop="$bin/prop"
+local engine="$bin/engine"
+local ax="[Λxeron]"
+local in="[-]"
+local war="[?]"
+
+. $fun
+. $prop
+. $engine
+
+echo "┌$ax $name | Changelogs V1.0"
+echo "└┐
+┌┘"
+echo "├$in AI Automaticaly Peformance in game"
+echo "├$in Render SkiaVk Priority"
+echo "├$in Gpu & Cpu Balance Performance"
+echo "└$in Fast-Conect To Server Modules"
