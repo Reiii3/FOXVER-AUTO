557584c81ad2b7ce13df820b8dbe5d16d255dd59
Reiii3 <mohtoyib79@gmail.com>
2025-03-22 18:18:29 +0700

    update run.sh

--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ local url_change="https://reiii3.github.io/FOXVER-AUTO/bin/changelogs.sh"
 local url_maintenance="https://reiii3.github.io/FOXVER-AUTO/bin/maintenance.sh"
 local url_main="https://reiii3.github.io/FOXVER-AUTO/engine/engine_main.sh"
 local url_ai="https://reiii3.github.io/FOXVER-AUTO/engine/core/ai-system.sh"
+local url_ai_reboot="https://reiii3.github.io/FOXVER-AUTO/engine/core/reboot-ai.sh"
 local function="$bin_2/function"
 local prop="$bin_2/prop"
 local engine="$bin_2/engine"
@@ -77,51 +78,7 @@ local su="[✔]"
 case $1 in
 # -upr adalah fungsi untuk merestart ulang ai agar dapat menjalankan game yang baru di tambahkan
     -upr | -u )
-    status=$(pgrep -f ai-system)
-    statuss=$(ps -fp $status)
-    if [ -n "$status" ]; then
-       echo "$in Reboot System.."
-       sleep 0.5
-       pkill -9 -f ai-system
-        while pgrep -f ai-system >/dev/null; do
-          echo "Menonaktifkan System AI"
-          sleep 1
-        done
-       rm "$ai"
-       echo "$su Reboot System succes "
-       am broadcast -a axeron.show.TOAST --es title "REBOOT SYSTEM AI" --es msg "SYSTEM AI DOWN" --ei duration "4000" >/dev/null 2>&1
-    fi
-    status=$(pgrep -f ai-system)
-     sleep 1
-     echo
-     echo "================================"
-     echo " ┌[📦] $name | UP PACKAGE" 
-     echo " └┬[📁] Package : ${packageRun:-null}"
-     if [ -z $status ]; then
-       echo "  └[📊] Status System : Reboot[🔄]"
-     else
-       echo "  └[📊] Status System : '$status'"
-     fi
-     echo "================================"
-     echo
-     sleep 2
-    if [ "$status" = "$pid_ins" ]; then
-      echo "$in Instalation gagal"
-    else 
-     echo "$pr Booting System"
-      sleep 0.7
-      storm -rP "$bin" -s "${url_ai}" -fn "ai-system" "$@"
-      nohup sh /data/local/tmp/ai-system >/dev/null 2>&1 &
-      sleep 1
-      status2=$(pgrep -f ai-system)
-      echo "$su Booting System succes"
-      am broadcast -a axeron.show.TOAST --es title "BOOTING SYSTEM AI" --es msg "Booting Succesfuly" --ei duration "4000" >/dev/null 2>&1
-    fi
-     axprop $engine pid_ins -s "$status2"
-     pid_ins=$status2
-     sleep 1
-     echo "ID AI : $pid_ins"
-     exit 0
+    
      ;;
      -info | -i )
       printer " ┌[📦] $name | INFORMATION"
