dbd18594f234f3fc6d18a3fe1d4adf67ddf4d8a5
Reiii3 <mohtoyib79@gmail.com>
2025-03-22 18:16:49 +0700

    update engine/core/reboot-ai.sh

--- engine/core/reboot-ai.sh
+++ engine/core/reboot-ai.sh
@@ -1,1 +1,45 @@
-
+  status=$(pgrep -f ai-system)
+    statuss=$(ps -fp $status)
+    if [ -n "$status" ]; then
+       echo "$in Reboot System.."
+       sleep 0.5
+       pkill -9 -f ai-system
+        while pgrep -f ai-system >/dev/null; do
+          echo "Menonaktifkan System AI"
+          sleep 1
+        done
+       rm "$ai"
+       echo "$su Reboot System succes "
+       am broadcast -a axeron.show.TOAST --es title "REBOOT SYSTEM AI" --es msg "SYSTEM AI DOWN" --ei duration "4000" >/dev/null 2>&1
+    fi
+    status=$(pgrep -f ai-system)
+     sleep 1
+     echo
+     echo "================================"
+     echo " ┌[📦] $name | UP PACKAGE" 
+     echo " └┬[📁] Package : ${packageRun:-null}"
+     if [ -z $status ]; then
+       echo "  └[📊] Status System : Reboot[🔄]"
+     else
+       echo "  └[📊] Status System : '$status'"
+     fi
+     echo "================================"
+     echo
+     sleep 2
+    if [ "$status" = "$pid_ins" ]; then
+      echo "$in Instalation gagal"
+    else 
+     echo "$pr Booting System"
+      sleep 0.7
+      storm -rP "$bin" -s "${url_ai}" -fn "ai-system" "$@"
+      nohup sh /data/local/tmp/ai-system >/dev/null 2>&1 &
+      sleep 1
+      status2=$(pgrep -f ai-system)
+      echo "$su Booting System succes"
+      am broadcast -a axeron.show.TOAST --es title "BOOTING SYSTEM AI" --es msg "Booting Succesfuly" --ei duration "4000" >/dev/null 2>&1
+    fi
+     axprop $engine pid_ins -s "$status2"
+     pid_ins=$status2
+     sleep 1
+     echo "ID AI : $pid_ins"
+     exit 0
