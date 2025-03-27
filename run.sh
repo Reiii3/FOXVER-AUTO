faf47f13a8aafd855e77a886392d6eae1742a6b6
Reiii3 <mohtoyib79@gmail.com>
2025-03-27 21:09:43 +0700

    update run.sh

--- run.sh
+++ run.sh
@@ -63,6 +63,9 @@ case $1 in
     echo "==================="
     printer " - version : $vers New"
     printer " - VersionCode : $versc New"
+    if [ ! -d "$bin_cash/zupdate_cek"]; then
+      mkdir -p "$bin_cash/zupdate_cek"
+    fi
     echo "updated=done" > "$bin_cash/zupdate_cek/update.txt"
     exit 0
     elif [ $beta_vers = $vers ] && [ $beta_versc = $versc ]; then
