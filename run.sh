d6be706b6eb4d003388cea09bf1ff9beca202ea7
Reiii3 <mohtoyib79@gmail.com>
2025-04-04 07:34:12 +0700

    update run.sh

--- run.sh
+++ run.sh
@@ -107,7 +107,9 @@ if [ ! -f "$engine" ] && [ ! -f "$prop" ] && [ ! -f "$function" ]; then
     storm -rP "$bin_2" -s "${url_fun}" -fn "function" "$@"
     storm -rP "$bin_2" -s "${url_main}" -fn "main" "$@"
     sleep 1
+  if [[ $debug = true ]]; then
     echo "all file dev ter ekstrak"
+  fi
 fi
 
 . $main
