c536f2a3bd7fe25887067d80eaaf4e0fd78ffc1c
Reiii3 <mohtoyib79@gmail.com>
2025-04-13 08:33:39 +0700

    update bin/maintenance.sh

--- bin/maintenance.sh
+++ bin/maintenance.sh
@@ -17,21 +17,21 @@ local su="[✔]"
 
 echo "Minetenance Tess"
 
-echo "      ========================"
+echo "      =========================="
 sleep 0.02
-echo "        Maintenance V1.1 XIO"
+echo "         Maintenance V1.1 XIO"
 sleep 0.02
-echo "      ========================"
+echo "      =========================="
 sleep 0.03
 echo "- Maintenance in 21 Mar 2025 12.30 WIB"
 sleep 0.03
 echo "- Update Version 1.1-XIO | 11"
 sleep 0.03
-printer "  [WHAT'S UPDATED]  "
-printer "  - New Function  "
-printer "  - Add New Code  "
-printer "  - Pengoptimalan skiaVk"
-printer "  - Fix Logic Modules  "
-printer "  - System Modules Update  "
-printer "  - AI Sensix Free Fire "
+printer "   [WHAT'S UPDATED]  "
+printer "   - New Function  "
+printer "   - Add New Code  "
+printer "   - Pengoptimalan skiaVk"
+printer "   - Fix Logic Modules  "
+printer "   - System Modules Update  "
+printer "   - AI Sensix Free Fire "
 echo
